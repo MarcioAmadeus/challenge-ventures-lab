5c0c2c6677c90a07a0221fedc08bd6ccd23d8153
Marcio Amadeus <marcioamadeus@gmail.com>
2024-03-21 22:47:03 -0300

    creation of the TaskManagement entity
    creation of the TaskManagement entity

--- back-end/src/DevIO.Business/Models/TaskManagement.cs
+++ back-end/src/DevIO.Business/Models/TaskManagement.cs
@@ -3,5 +3,16 @@
     public class TaskManagement : Entity
     {
         public Guid UserId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
     }
 }
