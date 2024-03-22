c7bdfc7a48aee2f2feb40cb53b6bcc447c37c1b8
Marcio Amadeus <marcioamadeus@gmail.com>
2024-03-21 22:52:27 -0300

    Update TaskValidation.cs
    A validation layer was also created with FluentValidation, leaving all fields mandatory.

--- back-end/src/DevIO.Business/Models/Validations/TaskValidation.cs
+++ back-end/src/DevIO.Business/Models/Validations/TaskValidation.cs
@@ -8,7 +8,22 @@ namespace DevIO.Business.Models.Validations
         {
             RuleFor(c => c.UserId)
                 .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
-            
+
+            RuleFor(c => c.Date)
+               .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(c => c.StartTime)
+              .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(c => c.EndTime)
+              .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(c => c.Subject)
+             .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(c => c.Description)
+             .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
         }
     }
 }
